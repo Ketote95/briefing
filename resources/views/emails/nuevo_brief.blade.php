0adba8db0dca2e82a61442885c92a905568dfc5f
ketote95 <Kennetheto1995@gmail.com>
2023-12-11 10:42:38 -0400

    Mejora del template de mails para briefs

--- resources/views/emails/nuevo_brief.blade.php
+++ resources/views/emails/nuevo_brief.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nuevo brief registrado</title>
+
+    {{-- Fuente de Google Poppins --}}
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
 <body>
-    <h2>Un nuevo brief ha sido registrado</h2>
-    <p><strong>Nombre de la Empresa: </strong>{{$nombre}}</p>
-    <p><strong>Tipo de Brief: </strong>{{$tipo}}</p>
-    <p>Ya está disponible su visualización en sistema</p>
+    <img src="<?php echo $_SERVER["DOCUMENT_ROOT"].'/storage/images/Header_briefs.jpeg';?>" width="100%" alt="Membrete top dosis">
+    <p style="font-family: 'Poppins'; color: #31297B;"><strong>Nombre de la Empresa: </strong><span style="color: #4D4D4D">{{$nombre}}</span></p>
+    <p style="font-family: 'Poppins'; color: #31297B;"><strong>Tipo de Brief: </strong><span style="color: #4D4D4D">{{$tipo}}</span></p>
+    <p style="font-family: 'Poppins'; color: #4D4D4D;">Ya está disponible su visualización en sistema</p>
 </body>
 </html>
